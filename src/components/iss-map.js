a87a05f61d62b4a0de3e1785097c9103edfaef72
Connor DiPietro <connor.dipietro@gmail.com>
2021-04-26 14:59:51 -0400

    fixes typos, swtiches location api to google reverse geocoding api

--- src/components/iss-map.js
+++ src/components/iss-map.js
@@ -9,7 +9,7 @@ const MapISS = () => {
   width="650"
   height="450"
   frameBorder="0" 
-  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD9reTsOYE-u-m03_Vr_nR49R2f13iNeSQ&q=${data.lang},${data.lat}&center=${data.lang},${data.lat}&zoom=2`} allowFullScreen>
+  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD9reTsOYE-u-m03_Vr_nR49R2f13iNeSQ&q=${data.lat},${data.lang}&center=${data.lat},${data.lang}&zoom=2`} allowFullScreen>
 </iframe>
   )
 }
--- src/reducers/location-reducer.js
+++ src/reducers/location-reducer.js
@@ -1,15 +1,14 @@
 import { FETCH_LOCATION } from "../actions";
 
 const DEFAULT_STATE = {
-  location: null
+  result: null
 }
 
 const LocationReducer = function(state = DEFAULT_STATE, action) {
   switch (action.type) {
     case FETCH_LOCATION:
       return {
-        location: [action.payload],
-        formatted: action.payload.data.results[0].formatted,
+        result: action.payload.data.results[0].formatted_address || 'Error',
       };
     default:
       return state;

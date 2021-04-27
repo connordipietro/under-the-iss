88bd57211418928dfd80e086eb24e86e3a582c5b
abeakaid <74392388+abeK021@users.noreply.github.com>
2021-04-27 09:13:33 -0400

    change for pull

--- src/components/iss-map.js
+++ src/components/iss-map.js
@@ -1,20 +1,16 @@
 import { useSelector } from 'react-redux';
-import GoogleMapReact from 'google-map-react'
-import {useState} from 'react'
-
-const AnyReactComponent = () => <div>text</div>;
 
 const MapISS = () => {
   const data = useSelector(state => state.data.coords);
    
   return (
     <iframe
-    title="ISS-LOCATION"
-  width="100%"
-  height="250"
-  frameBorder="0" 
-  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD9reTsOYE-u-m03_Vr_nR49R2f13iNeSQ&q=${data.lat},${data.lon}&center=${data.lat},${data.lon}&zoom=3`} allowFullScreen>
-</iframe>
+        title="ISS-LOCATION"
+        width="100%"
+        height="250"
+        frameBorder="0" 
+        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD9reTsOYE-u-m03_Vr_nR49R2f13iNeSQ&q=${data.lat},${data.lon}&center=${data.lat},${data.lon}&zoom=3`} allowFullScreen>
+    </iframe>
   )
 }
 
